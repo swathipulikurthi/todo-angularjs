a4c5307c8131082831b67b29fb6c2880297bd629
Pulikurthi <SWATHI.PULIKURTHI@thomsonreuters.com>
2019-02-18 18:44:45 +0530

    added delete and strike functionality using model

--- app/controller/todo-controller.js
+++ app/controller/todo-controller.js
@@ -2,22 +2,42 @@ angular.module('todoApp').controller('TasksController', ['$scope',
 function ($scope) {
   
   $scope.vm = {};
-  $scope.checked=false;
-  $scope.vm.taskName = null;
-
+ 
+  
+  
   $scope.vm.tasks = [];
-  $scope.vm.task_close=null;
+  $scope.checked=false;
+ 
 
   $scope.vm.addTask = function addTask() {
-    if($scope.vm.taskName) {
-      $scope.vm.tasks.push($scope.vm.taskName);
-      $scope.vm.taskName = null;
+    //console.log($scope.input);
+    if($scope.input) {
+      $scope.vm.tasks.push({Name:$scope.input, checked:$scope.checked});
+     //console.log($scope.vm.tasks);
+      $scope.input= null;
     }
   };
+
+
+  
+
+  
   $scope.vm.removeTask=function removeTask(taskName){
-    //console.log(taskName);
+    
     if(taskName) {
+     // console.log($scope.vm.tasks.indexOf(taskName));
       $scope.vm.tasks.splice($scope.vm.tasks.indexOf(taskName), 1);
+     // console.log(taskName);
     }
   }
+
+  // $scope.vm.strikeTask=function strikeTask(taskName){
+  
+  //   if(taskName) {
+  //     var p= $scope.vm.tasks.indexOf(taskName);
+  //     console.log(p);
+  //     $scope.vm.tasks[p].checked=!$scope.vm.tasks[p].checked  ;
+  //     console.log(taskName);
+  //   }
+  // }
 }]);
